df0341d5b389b701dfdf44e5507e8d555ce7274c
Eazcee <edgar.zcanas@gmail.com>
2026-04-07 19:41:08 -0400

    -add GitHub Actions CI lint workflow for HTML CSS and JS

---
+++ stylelint.config.mjs
@@ -0,0 +1,11 @@
+export default {
+  ignoreFiles: ["**/node_modules/**"],
+  extends: ["stylelint-config-standard"],
+  rules: {
+    "alpha-value-notation": null,
+    "color-hex-length": null,
+    "font-family-name-quotes": null,
+    "rule-empty-line-before": null,
+    "selector-id-pattern": null
+  }
+};
